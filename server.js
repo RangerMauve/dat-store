60c3fe8cd2f0b8dc13167301426f198bf75b28b2
RangerMauve <ranger@mauve.moe>
2019-08-20 14:00:05 -0400

    Progress on exposing dat-store

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const pda = require('pauls-dat-api')
 const DSS = require('discovery-swarm-stream/server')
 const DAT_SWARM_DEFAULTS = require('dat-swarm-defaults')
 const envPaths = require('env-paths')
+const tsse = require('tsse')
 
 const Library = require('./library')
 
 const DEFAULT_STORAGE_LOCATION = envPaths('dat-store').data
 const DEFAULT_PORT = 3472
-const DEFAULT_HOST = '::'
+const DEFAULT_HOST = 'localhost'
+const DEFAULT_ANY_HOST = '::'
 
 const ERROR_NOT_LOCAL = (path, ip) => `Cannot Add File Path From Remote IP ${path}, ${ip}`
+const ERROR_INVALID_CREDENTIALS = 'Please supply valid username and password credentials'
 
 module.exports =
 
@@ -23,7 +26,18 @@ class StoreServer {
     return server
   }
 
-  async init ({ port, host, storageLocation, verbose = true, datPort = 3282, latest = false }) {
+  async init ({
+    port,
+    host,
+    storageLocation,
+    verbose = true,
+    datPort = 3282,
+    latest = false,
+    allowCors,
+    exposeToInternet,
+    authenticationUsername,
+    authenticationPassword
+  }) {
     storageLocation = storageLocation || DEFAULT_STORAGE_LOCATION
 
     this.library = new Library({
@@ -33,6 +47,37 @@ class StoreServer {
     this.dir = storageLocation
 
     this.fastify = createFastify({ logger: verbose })
+
+    // Enable websites to make requests to the store from any origin
+    if (allowCors) {
+      this.fastify.register(require('fastify-cors'), {
+        origin: '*',
+        methods: ['GET', 'PUT', 'POST']
+      })
+    }
+
+    // Listen on _all_ network interfaces
+    if (exposeToInternet) {
+      host = DEFAULT_ANY_HOST
+    }
+
+    if (authenticationUsername || authenticationPassword) {
+      this.fastify.register(require('fastify-basic-auth'), {
+        validate: (username, password) => {
+          // If a username is required for authentication, check it
+          // Use constant time string comparison which is important for security
+          if (authenticationUsername && !tsse(authenticationUsername, username)) {
+            throw new Error(ERROR_INVALID_CREDENTIALS)
+          }
+
+          // If a password was required for authentication, check it
+          if (authenticationPassword && !tsse(authenticationPassword, password)) {
+            throw new Error(ERROR_INVALID_CREDENTIALS)
+          }
+        },
+        authenticate: true
+      })
+    }
 
     const swarmOpts = DAT_SWARM_DEFAULTS({
       hash: false
@@ -55,90 +100,97 @@ class StoreServer {
   }
 
   initRoutes () {
-    this.fastify.get('/.well-known/psa', async () => {
-      return {
-        'PSA': 1,
-        'title': 'My ',
-        'description': 'Keep your Dats online!',
-        'links': [{
-          'rel': 'https://archive.org/services/purl/purl/datprotocol/spec/pinning-service-account-api',
-          'title': 'User accounts API',
-          'href': '/v1/accounts'
-        }, {
-          'rel': 'https://archive.org/services/purl/purl/datprotocol/spec/pinning-service-dats-api',
-          'title': 'Dat pinning API',
-          'href': '/v1/dats'
-        }]
-      }
-    })
-
-    this.fastify.post('/v1/accounts/login', async () => {
-      return {
-        sessionToken: 'null'
-      }
-    })
-
-    this.fastify.post('/v1/accounts/logout', async () => {
-      return {}
-    })
-
-    this.fastify.get('/v1/accounts/account', async () => {
-      return {
-        username: 'Localhost'
-      }
-    })
-
-    this.fastify.get('/v1/dats/', async () => {
-      const keys = await this.library.list()
-
-      const items = []
-
-      for (let key of keys) {
-        const metadata = await this.getMetadata(key)
-
-        items.push(metadata)
+    this.fastify.after(() => {
+      // Add in the basic auth hook if it exists
+      if (this.fastify.basicAuth) {
+        this.fastify.addHook('preHandler', this.fastify.basicAuth)
       }
 
-      return {
-        items
-      }
-    })
+      this.fastify.get('/.well-known/psa', async () => {
+        return {
+          'PSA': 1,
+          'title': 'My ',
+          'description': 'Keep your Dats online!',
+          'links': [{
+            'rel': 'https://archive.org/services/purl/purl/datprotocol/spec/pinning-service-account-api',
+            'title': 'User accounts API',
+            'href': '/v1/accounts'
+          }, {
+            'rel': 'https://archive.org/services/purl/purl/datprotocol/spec/pinning-service-dats-api',
+            'title': 'Dat pinning API',
+            'href': '/v1/dats'
+          }]
+        }
+      })
 
-    this.fastify.post('/v1/dats/add', async ({ body, ip }) => {
-      const { url } = body
+      this.fastify.post('/v1/accounts/login', async () => {
+        return {
+          sessionToken: 'null'
+        }
+      })
 
-      if (url.startsWith('dat://')) {
-        await this.library.addURL(url)
-      } else {
-        if (!ip.endsWith('127.0.0.1')) { throw new Error(ERROR_NOT_LOCAL(url, ip)) }
-        await this.library.addFolder(url)
-      }
-      return {}
-    })
+      this.fastify.post('/v1/accounts/logout', async () => {
+        return {}
+      })
 
-    this.fastify.post('/v1/dats/remove', async ({ body, ip }) => {
-      try {
+      this.fastify.get('/v1/accounts/account', async () => {
+        return {
+          username: 'Localhost'
+        }
+      })
+
+      this.fastify.get('/v1/dats/', async () => {
+        const keys = await this.library.list()
+
+        const items = []
+
+        for (let key of keys) {
+          const metadata = await this.getMetadata(key)
+
+          items.push(metadata)
+        }
+
+        return {
+          items
+        }
+      })
+
+      this.fastify.post('/v1/dats/add', async ({ body, ip }) => {
         const { url } = body
 
         if (url.startsWith('dat://')) {
-          await this.library.removeURL(url)
+          await this.library.addURL(url)
         } else {
           if (!ip.endsWith('127.0.0.1')) { throw new Error(ERROR_NOT_LOCAL(url, ip)) }
-          await this.library.removeFolder(url)
+          await this.library.addFolder(url)
         }
-      } catch (e) { console.error(e) }
+        return {}
+      })
 
-      return {}
-    })
+      this.fastify.post('/v1/dats/remove', async ({ body, ip }) => {
+        try {
+          const { url } = body
 
-    this.fastify.get('/v1/dats/item/:key', async ({ params }) => {
-      const { key } = params
+          if (url.startsWith('dat://')) {
+            await this.library.removeURL(url)
+          } else {
+            if (!ip.endsWith('127.0.0.1')) { throw new Error(ERROR_NOT_LOCAL(url, ip)) }
+            await this.library.removeFolder(url)
+          }
+        } catch (e) { console.error(e) }
 
-      return this.getMetadata(key)
-    })
+        return {}
+      })
 
-    this.fastify.post('/v1/dats/item/:key', async () => {
-      return {}
+      this.fastify.get('/v1/dats/item/:key', async ({ params }) => {
+        const { key } = params
+
+        return this.getMetadata(key)
+      })
+
+      this.fastify.post('/v1/dats/item/:key', async () => {
+        return {}
+      })
     })
   }
 
--- test.js
+++ test.js
@@ -71,7 +71,7 @@ test('Talk to server with client', async (t) => {
     t.equals(persistedProviderURL, providerURL, 'Provider service persisted')
 
     const DAT_PROJECT_KEY = 'dat://60c525b5589a5099aa3610a8ee550dcd454c3e118f7ac93b7d41b6b850272330'
-    const DAT_PROJECT_DOMAIN = 'dat://datproject.org'
+    const DAT_PROJECT_DOMAIN = 'dat://dat.foundation'
 
     await client.add(DAT_PROJECT_DOMAIN)
 
@@ -127,10 +127,10 @@ test('Talk to server with client', async (t) => {
     const exampleFileLocation = path.join(hyperdriveLocation, exampleFileName)
     const exampleFileData = 'Hello World'
 
-    fs.writeFile(exampleFileLocation, exampleFileData)
+    await fs.writeFile(exampleFileLocation, exampleFileData)
 
     // Wait for the archive to detect the write
-    await new Promise((resolve) => setTimeout(resolve, 500))
+    await new Promise((resolve) => setTimeout(resolve, 1000))
 
     await client.remove(hyperdriveLocation)
 
