0bf0111d166b5aaea2c5b7bc66768c3d697ffb37
mauve <ranger@mauve.moe>
2022-02-17 12:54:04 -0500

    Fix up build, log build errors

--- build.js
+++ build.js
@@ -3,45 +3,50 @@ const args = require('yargs')
   .demandOption('target')
   .parse(process.argv.slice(2))
 
-const {name} = require('./package.json')
+try {
+  const { name } = require('./package.json')
 
-const OUTPUT_MAP = {
-  linux: `./dist/${name}-linux`,
-  darwin: `./dist/${name}-macos`,
-  win32: `./dist/${name}-windows.exe`
+  const OUTPUT_MAP = {
+    linux: `./dist/${name}-linux`,
+    darwin: `./dist/${name}-macos`,
+    win32: `./dist/${name}-windows.exe`
+  }
+
+  const packageData = require('./package.json')
+
+  const { target } = args
+  const targetKey = `pkg-${target}`
+
+  const pkgConfig = packageData[targetKey]
+
+  if (!pkgConfig) throw new Error(`Invalid target, must supply ${targetKey} in the package.json`)
+
+  packageData.pkg = pkgConfig
+
+  console.log('Preparing package.json')
+
+  require('fs').writeFileSync('./package.json', JSON.stringify(packageData, null, '  '))
+
+  console.log('Rebuilding dependencies for', target)
+
+  require('child_process').execSync(`npm i --target_arch=x64 --target_platform=${target}`, { stdio: ['pipe', 'pipe', 'pipe'] })
+
+  const outputName = OUTPUT_MAP[target] || pkgConfig.output
+
+  if (!outputName) throw new Error('Must specify appropriate output name in config')
+
+  console.log('Compiling, output to', outputName)
+
+  require('child_process').execSync(`pkg ./ --output ${outputName} --compress Brotli`, { stdio: ['pipe', 'pipe', 'pipe'] })
+
+  console.log('Cleaning package.json')
+
+  delete packageData.pkg
+
+  require('fs').writeFileSync('./package.json', JSON.stringify(packageData, null, '  '))
+
+  console.log('Done!')
+} catch (e) {
+  if (e.stdout) console.error(e.stdout.toString('utf8'))
+  else throw e
 }
-
-const packageData = require('./package.json')
-
-const { target } = args
-const targetKey = `pkg-${target}`
-
-const pkgConfig = packageData[targetKey]
-
-if (!pkgConfig) throw new Error(`Invalid target, must supply ${targetKey} in the package.json`)
-
-packageData.pkg = pkgConfig
-
-console.log('Preparing package.json')
-
-require('fs').writeFileSync('./package.json', JSON.stringify(packageData, null, '  '))
-
-console.log('Rebuilding dependencies for', target)
-
-require('child_process').execSync(`npm i --target_arch=x64 --target_platform=${target}`, { stdio: ['pipe', 'pipe', 'pipe'] })
-
-const outputName = OUTPUT_MAP[target] || pkgConfig.output
-
-if (!outputName) throw new Error('Must specify appropriate output name in config')
-
-console.log('Compiling, output to', outputName)
-
-require('child_process').execSync(`pkg ./ --output ${outputName} --compress Brotli`, { stdio: ['pipe', 'pipe', 'pipe'] })
-
-console.log('Cleaning package.json')
-
-delete packageData.pkg
-
-require('fs').writeFileSync('./package.json', JSON.stringify(packageData, null, '  '))
-
-console.log('Done!')
